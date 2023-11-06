3090714bb4f01915571fbe30d58873f0e1921912
Sedov <namesjoe@gmail.com>
2023-11-06 18:04:14 +0300

    add config

--- src/core/config.py
+++ src/core/config.py
@@ -2,13 +2,25 @@ from os.path import join, dirname
 
 from dotenv import load_dotenv
 
-from src.core.settings import Settings
 from src.utils.utils_funcs import get_db_url
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    SERVER_HOST: str
+    SERVER_PORT: int
+    JWT_SECRET_KEY: str
+    DBNAME: str
+    HOST_URL: str
+
+    class Config:
+        env_file = ".env"
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-settings = Settings()  # Create an instance of the Settings class
+settings = Settings()
 
 SERVER_HOST = settings.SERVER_HOST
 SERVER_PORT = settings.SERVER_PORT
