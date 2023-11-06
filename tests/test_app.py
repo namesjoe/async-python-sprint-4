dbbcec10a438659a040c0abc72055a85ce8d35ac
Sedov <namesjoe@gmail.com>
2023-11-06 18:45:24 +0300

    tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,19 @@
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
+import pytest
 
-# Import the app fixture from conftest
 from .conftest import app
 
 
-# Your test functions
-def test_read_root(test_client: TestClient):
-    response = test_client.get(app.url_path_for("read_root"))
-    assert response.status_code == 200
-    assert response.json() == {"message": "Short link here!"}
+@pytest.mark.asyncio
+async def test_read_root(test_client: TestClient):
+    async with AsyncClient(app=app, base_url="http://testserver") as ac:
+        response = await ac.get(app.url_path_for("read_root"))
+        assert response.status_code == 200
+        assert response.json() == {"message": "Short link here!"}
 
-
-def test_shorten_url_without_authentication(test_client: TestClient):
-    response = test_client.post(app.url_path_for("shorten"),
-                                json={"url": "http://google.com"})
-    assert response.status_code == 401
+@pytest.mark.asyncio
+async def test_shorten_url_without_authentication(test_client: TestClient):
+    async with AsyncClient(app=app, base_url="http://testserver") as ac:
+        response = await ac.post(app.url_path_for("shorten"), json={"url": "http://google.com"})
+        assert response.status_code == 401
--- tests/test_db.py
+++ tests/test_db.py
@@ -3,62 +3,59 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define your SQLAlchemy models
+from src.models.model import URL
+
+
 Base = declarative_base()
 
 
-class URL(Base):
-    __tablename__ = 'urls'
-
-    id = Column(Integer, primary_key=True)
-    full_url = Column(String)
-    short_url_id = Column(String)
-    user_id = Column(Integer)
-    deleted = Column(Boolean, default=False)
-
-
-# test database and session
-@pytest.fixture
-def db_session():
+@pytest.mark.asyncio
+async def test_create_url():
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    yield session
+
+    url = URL(
+        full_url="http://example.com",
+        short_url_id="test-short-url",
+        user_id=1,
+        deleted=False
+    )
+    session.add(url)
+    session.commit()
+
+    retrieved_url = session.query(URL).filter_by(short_url_id="test-short-url").first()
+    assert retrieved_url is not None
+    assert retrieved_url.full_url == "http://example.com"
+
     session.close()
     Base.metadata.drop_all(engine)
 
 
-# Example test for creating a URL
-def test_create_url(db_session):
+@pytest.mark.asyncio
+async def test_delete_url():
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     url = URL(
         full_url="http://example.com",
         short_url_id="test-short-url",
         user_id=1,
         deleted=False
     )
-    db_session.add(url)
-    db_session.commit()
-
-    retrieved_url = db_session.query(URL).filter_by(short_url_id="test-short-url").first()
-    assert retrieved_url is not None
-    assert retrieved_url.full_url == "http://example.com"
-
-
-# Example test for deleting a URL
-def test_delete_url(db_session):
-    url = URL(
-        full_url="http://example.com",
-        short_url_id="test-short-url",
-        user_id=1,
-        deleted=False
-    )
-    db_session.add(url)
-    db_session.commit()
+    session.add(url)
+    session.commit()
 
     # Delete the URL
-    db_session.delete(url)
-    db_session.commit()
+    session.delete(url)
+    session.commit()
 
-    retrieved_url = db_session.query(URL).filter_by(short_url_id="test-short-url").first()
+    retrieved_url = session.query(URL).filter_by(short_url_id="test-short-url").first()
     assert retrieved_url is None
+
+    session.close()
+    Base.metadata.drop_all(engine)
+
